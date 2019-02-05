c74f2592923e94d468c697f18417c44ba247cc01
Lehchibi Gaël <lehchibi.gael@gmail.com>
2019-02-05 16:06:47 +0100

    Add Main with new Game

---
+++ src/com/metaxiii/Main.java
@@ -0,0 +1,11 @@
+package com.metaxiii;
+
+import com.metaxiii.Game.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.init();
+    }
+}
