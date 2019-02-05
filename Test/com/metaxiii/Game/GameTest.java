fbf663e9e4cdbf2083ba8c0e3d93451e82800836
Lehchibi Gaël <lehchibi.gael@gmail.com>
2019-02-05 16:09:40 +0100

    Uncomplete GameTest

---
+++ Test/com/metaxiii/Game/GameTest.java
@@ -0,0 +1,24 @@
+package com.metaxiii.Game;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+    Game game = new Game();
+}
